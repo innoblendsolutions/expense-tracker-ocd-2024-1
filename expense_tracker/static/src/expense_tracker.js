826ff0762f037599a13d7ade81a3c3f3004a0d07
Mohammed Shekha <msh@odoo.com>
2024-08-07 17:46:24 +0530

    ss

--- expense_tracker/static/src/expense_tracker.js
+++ expense_tracker/static/src/expense_tracker.js
@@ -5,7 +5,10 @@ import { Container } from "./components/container/container";
 export class ExpenseTracker extends Component {
     static template = "expense_tracker.root";
 <<<<<<< HEAD
+<<<<<<< HEAD
     static components = { Header, Container };
+=======
+>>>>>>> 5a3c4c1 ([Solution] 1.2 Add template to display hello world component)
 =======
 >>>>>>> 5a3c4c1 ([Solution] 1.2 Add template to display hello world component)
 }
